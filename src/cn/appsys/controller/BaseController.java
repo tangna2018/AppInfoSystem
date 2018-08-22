340084ee51d7833bead39d37ab29dacafcdf8dcc
Administrator <Administrator@LMQCVI0FNFWPPCR>
2018-08-22 15:41:33 +0800

    lll

--- src/cn/appsys/controller/BaseController.java
+++ src/cn/appsys/controller/BaseController.java
@@ -6,7 +6,7 @@ import java.util.Date;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
-
+//时间转换
 public class BaseController {
 	/**
 	 * CustomDateEditor 自定义的日期编辑器
